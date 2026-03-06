fa199622527312c2750450bd7b82aa6f5c3c108d
nusi111 <bakisnus@students.zhaw.ch>
2026-03-06 14:21:50 +0100

    fix code

--- src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
+++ src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
@@ -2,18 +2,31 @@ package ch.zhaw.avalanger.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import ch.zhaw.avalanger.model.Avalange;
 
 @RestController
 @RequestMapping("/api/avalange")
 public class AvalangeController {
 
     @GetMapping("/{country}")
-    public String getAllAvalanges(@PathVariable String country,
-                                    @RequestParam(required = false) String state
-    ) {
-        return "No avalanges found for country: "+country+ ", state: "+ state;
+    public String getAllAvalanges(@PathVariable(required = false) String country,
+            @RequestParam(required = false) String state) {
+        if (country == null || country.isEmpty()) {
+            return "No avalanges found";
+        }
+        return "No avalanges found for country: " + country + ", state: " + state;
+    }
+
+    @PostMapping("")
+    public String postMethodName(@RequestBody Avalange avalange) {
+
+        return "Avalange received: " + avalange.getCountry() + ", " + avalange.getState() + ", "
+                + avalange.getDescription();
     }
 }
